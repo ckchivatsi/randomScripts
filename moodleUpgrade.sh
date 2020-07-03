8efd7e2422ecdae353872b1afbb87f5ef154c700
ckchivatsi <kelvinchivatsi@hotmail.com>
2020-07-03 19:25:35 +0300

    Moodle site upgrade script(3.8-3.9)

---
+++ moodleUpgrade.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#This is a quick script to upgrade Moodle LMS.
+#Still needs alot of work to make it interactive and also include tests to ensure it does not fail.
+#####!!!!!WARNING!!!!!#####
+#This script has some disastrous commands that may leave you crying; kindly go through it and make sure it will be doing what you expect.
+#I am not liable for any damages or losses of data incured
+#Edit the line containing theme and mods(plugins) to the ones you installed on your system
+##you can find the list of your additional plugins from Site Administration > Plugins > Plugins Overview > Additional Plugins
+#Change version numbers to your preference
+
+#Upgrade Moodle 3.8 to Moodle 3.9
+php /var/www/html/moodle/admin/cli/maintenance.php --enable
+cd /var/www/html
+mv moodle moodle.bak
+cd /opt/moodle
+git pull
+git branch --track MOODLE_39_STABLE origin/MOODLE_39_STABLE
+git checkout MOODLE_39_STABLE
+git branch -D MOODLE_38_STABLE
+cp -R /opt/moodle /var/www/html
+chmod -R 0755 /var/www/html/moodle
+cd /var/www/html
+cp moodle.bak/config.php moodle/config.php
+#themes and mods
+cp -pr moodle.bak/theme/moove moodle/theme/moove
+cp -pr moodle.bak/mod/attendance moodle/mod/attendance
+cp -pr moodle.bak/mod/bigbluebuttonbn moodle/mod/bigbluebuttonbn
+cp -pr moodle.bak/mod/jitsi moodle/mod/jitsi
+cp -pr moodle.bak/mod/livepoll moodle/mod/livepoll
+cp -pr moodle.bak/report/extendedlog moodle/report/extendedlog
+cp -pr moodle.bak/blocks/dedication moodle/blocks/dedication
+cp -pr moodle.bak/question/type/essaywiris moodle/question/type/essaywiris
+cp -pr moodle.bak/question/type/matchwiris moodle/question/type/matchwiris
+cp -pr moodle.bak/question/type/multianswerwiris moodle/question/type/multianswerwiris
+cp -pr moodle.bak/question/type/multichoicewiris moodle/question/type/multichoicewiris
+cp -pr moodle.bak/question/type/shortanswerwiris moodle/question/type/shortanswerwiris
+cp -pr moodle.bak/question/type/truefalsewiris moodle/question/type/truefalsewiris
+cp -pr moodle.bak/question/type/wq moodle/question/type/wq
+cp -pr moodle.bak/filter/wiris moodle/filter/wiris
+cp -pr moodle.bak/lib/editor/atto/plugins/wiris moodle/lib/editor/atto/plugins/wiris
+cp -pr moodle.bak/lib/editor/tinymce/plugins/tiny_mce_wiris moodle/lib/editor/tinymce/plugins/tiny_mce_wiris
+cp -pr moodle.bak/local/wirisquizzes moodle/local/wirisquizzes
+##
+chown -R root:root moodle
+chmod -R 0755 moodle
+cd moodle
+php admin/cli/upgrade.php -y
+php admin/cli/maintenance.php --disable
